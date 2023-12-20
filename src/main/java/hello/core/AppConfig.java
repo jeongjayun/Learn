057bf04d6baac4ac56648738e26ee37dc5d555dc
jeongjayun <116062065+jeongjayun@users.noreply.github.com>
2023-12-20 17:40:10 +0900

    20231220

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,26 +1,26 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // 설정정보
 public class AppConfig {
-    @Bean
-    public MemberService memberService(){
+    @Bean(name = "memberService")
+    public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    // 빈 이름
+    // 메서드 이름을 사용한다. 위처럼 name ="" 으로 이름 지정해줄 수 있음.
+    // 빈 이름은 항상 다른 이름을 부여해야 한다. 같은 이름을 부여하면 다른 빈이 무시되거나 기존 빈을 덮어버리는 등 설정에 따라 오류가 발생한다. 실무에서는 단순하고 명확하게 개발해야 한다.
+
     @Bean
-    public MemoryMemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
