223040b35b97a35662345210603029e5ee6b1450
jeongjayun <1252564@gmail.com>
2024-01-07 20:26:19 +0900

    .

--- java-start/src/CommentJava.java
+++
@@ -1,8 +0,0 @@
-public class CommentJava {
-    public static void main(String[] args) {
-        System.out.println("hello java1"); //hello java1 을 출력합니다.
-        // System.out.println("hello java2"); //hello java2 을 출력합니다.
-        /*System.out.println("hello java3");
-        System.out.println("hello java4");*/
-    }
-}
--- java-start/src/variable/Var1.java
+++
@@ -1,9 +0,0 @@
-package variable;
-
-public class Var1 { // 클래스 이름은 관례상 첫글자는 대문자
-    public static void main(String[] args) {
-        System.out.println(20); // 변경 10->20
-        System.out.println(20); // 변경 10->20
-        System.out.println(20); // 변경 10->20
-    }
-}
